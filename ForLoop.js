68d76746b3e22d9743c80dfd5ca50afa6be1f3a9
Judith Mueni <judithmueni395@gmail.com>
2018-02-17 00:29:03 +0300

    Learning Js

---
+++ ForLoop.js
@@ -0,0 +1,5 @@
+var DaysOfTheWeek = 7;
+
+for(i=0; i <= DaysOfTheWeek; i++){
+console.log(i)
+}
---
+++ variables.js
@@ -0,0 +1,18 @@
+var classSize = 23;
+
+// add boolean
+
+var isWeekday = false;
+
+// add string
+
+var className = "Akirachix";
+
+// add float
+
+var shoeSize = 5;
+
+// this is used to print
+
+console.log(classSize)
+
