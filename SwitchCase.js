a0831fc8b5ed839e1908112809e93d5ed44c9ef8
Judith Mueni <judithmueni395@gmail.com>
2018-03-01 06:41:13 +0300

    added SwitchCase

---
+++ SwitchCase.js
@@ -0,0 +1,15 @@
+var isSunday = false
+
+switch(isSunday){
+	case true:
+	console.log("Today is sunday")
+	break;
+
+	case false:
+	console.log("Today is not sunday")
+	break;
+
+	default:
+	console.log("Error")
+	break;
+}
