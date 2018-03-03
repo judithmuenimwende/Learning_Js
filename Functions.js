75ffdff12bfd1a2d38d6ce74d4e654e181b91720
Judith Mueni <judithmueni395@gmail.com>
2018-03-03 09:14:33 +0300

    Functions# git commit -m Functions

---
+++ Functions.js
@@ -0,0 +1,50 @@
+// This is a void function
+		//ADDING
+function add (){
+	var numberone = 1;
+	var numbertwo = 2;
+
+	var sum = numberone + numbertwo;
+		console.log (sum);
+}
+//call function
+add()
+
+			//MULTIPLYING
+function multiply (){
+	var numberone = 5;
+	var numbertwo = 8;
+	var numberthree = 7;
+
+	var answer = numberone * numbertwo * numberthree;
+		
+		return answer;
+}
+var result = multiply()
+	console.log(result)
+
+
+					//SUBTRACTING
+	function subtraction(){
+		var numberone = 40;
+		var numbertwo = 36;
+
+		var answer = numberone - numbertwo;
+			return answer;
+	}
+var result = subtraction();
+	console.log(result)
+
+
+						//DIVIDING
+		function divide(){
+			var numberone = 8;
+			var numbertwo = 4;
+
+			var answer = numberone/numbertwo;
+				return answer;
+	}
+var result = divide()
+	console.log(result)
+
+
